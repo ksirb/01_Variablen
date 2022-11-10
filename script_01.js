09f71437489a6689a88560e36b96d6d01367ad8f
Mia <mia.kamenetski@online.de>
2022-11-10 09:22:00 +0100

    IF

--- script_01.js
+++ script_01.js
@@ -70,20 +70,33 @@
 
 /***** 05 Entscheidungsstrukturen | Control flow *****/
 
-//Deklaration + Assignment
-const ageJohn = 30;
+// //Deklaration + Assignment
+const ageJohn = 25;
 const ageMark = 30;
 
-//Deklaration
+// //Deklaration
 let isJohnOlder, isJohnEqual;
 
-//Test | Logische Aussage
+// //Test | Logische Aussage
 isJohnOlder = (ageJohn > ageMark);
 isJohnEqual = (ageJohn == ageMark);
 
-//Ausgabe
-console.log("ageJohn: " + ageJohn);
-console.log("ageMark: " + ageMark);
-console.log("isJohnOlder: " + isJohnOlder);
-console.log("isJohnEqual: " + isJohnEqual);
-console.log("------------------");
+// //Ausgabe
+// console.log("ageJohn: " + ageJohn);
+// console.log("ageMark: " + ageMark);
+// console.log("isJohnOlder: " + isJohnOlder);
+// console.log("isJohnEqual: " + isJohnEqual);
+// console.log("------------------");
+
+/***** IF *****/
+//TINA --> There is no alternative!
+//entwieder JA oder nix... alternativlos
+
+//if(true) // always true
+//if(false) // always false
+// if(isJohnOlder) // proof
+// if(ageJohn > ageMark) // proof_2
+// {
+//    console.log("John ist älter.");
+// }
+
